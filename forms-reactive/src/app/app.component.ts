67648f10d8ca64432b011ee8cbe7c98eaafb3de1
kasunweerasinghe <kasunrachintha2018@gmail.com>
2023-07-25 14:31:08 +0530

    create async validator for check is email is correct or not

--- forms-reactive/src/app/app.component.ts
+++ forms-reactive/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {FormArray, FormControl, FormGroup, Validators} from "@angular/forms";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -20,7 +21,7 @@ export class AppComponent implements OnInit {
       'userData': new FormGroup({
         //we can add some values like 'kasun' as a username but here its null. Validators used for validation
         'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
-        'email': new FormControl(null, [Validators.required, Validators.email]),
+        'email': new FormControl(null, [Validators.required, Validators.email], this.forbiddenEmails),
       }),
       'gender': new FormControl('male'),
       // this holds arrays of form controls
@@ -43,6 +44,20 @@ export class AppComponent implements OnInit {
       return {'nameIsForbidden': true};
     }
     return null;
+  }
+
+  // create async validator for check is email is correct or not
+  forbiddenEmails(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'test@gmail.com') {
+          resolve({'emailIsForbidden': true})
+        } else {
+          resolve(null);
+        }
+      }, 1500)
+    });
+    return promise;
   }
 
 }
