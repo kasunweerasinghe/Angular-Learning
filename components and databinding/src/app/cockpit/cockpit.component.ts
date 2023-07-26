4339d81b6922008bce0afede3e42189daec86078
kasunweerasinghe <kasunrachintha2018@gmail.com>
2023-07-26 10:33:24 +0530

    add @ViewChild

--- components and databinding/src/app/cockpit/cockpit.component.ts
+++ components and databinding/src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, ElementRef, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -10,7 +10,10 @@ export class CockpitComponent implements OnInit {
   @Output() serverCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
   @Output('bpCreated') blueprintCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
   // newServerName = '';
-  newServerContent = '';
+  // newServerContent = '';
+
+  // serverContentInput is local reference name pass as string without using 2 way binding
+  @ViewChild('serverContentInput') serverContentInput: ElementRef
 
   constructor() {
   }
@@ -19,12 +22,14 @@ export class CockpitComponent implements OnInit {
   }
 
   onAddServer(nameInput: HTMLInputElement) {
-    console.log(nameInput.value)
-    this.serverCreated.emit({serverName: nameInput.value, serverContent: this.newServerContent});
+    console.log(this.serverContentInput)
+    // this.serverCreated.emit({serverName: nameInput.value, serverContent: this.newServerContent});
+    this.serverCreated.emit({serverName: nameInput.value, serverContent: this.serverContentInput.nativeElement.value});
   }
 
   onAddBlueprint(nameInput: HTMLInputElement) {
-    this.blueprintCreated.emit({serverName: nameInput.value, serverContent: this.newServerContent});
+    // this.blueprintCreated.emit({serverName: nameInput.value, serverContent: this.newServerContent});
+    this.blueprintCreated.emit({serverName: nameInput.value, serverContent: this.serverContentInput.nativeElement.value});
   }
 
 }
