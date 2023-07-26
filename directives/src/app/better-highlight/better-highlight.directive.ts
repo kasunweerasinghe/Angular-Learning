fbdf2bdc4d5905736d0bd1f4b7ac06cfba807bcf
kasunweerasinghe <kasunrachintha2018@gmail.com>
2023-07-26 15:43:04 +0530

    content update

--- directives/src/app/better-highlight/better-highlight.directive.ts
+++ directives/src/app/better-highlight/better-highlight.directive.ts
@@ -1,11 +1,16 @@
-import {Directive, ElementRef, OnInit, Renderer2,HostListener} from '@angular/core';
+import {Directive, ElementRef, OnInit, Renderer2, HostListener, HostBinding} from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
 
+  // another way other than @HostListener
+  @HostBinding('style.backgroundColor') backgroundColor: string = 'transparent';
+  @HostBinding('style.color') color: string = 'black';
 
+
+  // Renderer2 use with @HostListener
   constructor(private elRef: ElementRef, private renderer: Renderer2) {
   }
 
@@ -14,15 +19,26 @@ export class BetterHighlightDirective implements OnInit {
     // this.renderer.setStyle(this.elRef.nativeElement, 'color', 'white');
   }
 
-  // create a event like hover.
-  @HostListener('mouseenter') mouseover(eventData: Event){
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
-    this.renderer.setStyle(this.elRef.nativeElement, 'color', 'white');
+  // create events like hover.
+  @HostListener('mouseenter') mouseover(eventData: Event) {
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+    // this.renderer.setStyle(this.elRef.nativeElement, 'color', 'white');
+
+
+    // using @HostBinding
+    this.backgroundColor = 'blue';
+    this.color = 'white';
+
   }
 
-  @HostListener('mouseleave') mouseleave(eventData: Event){
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
-    this.renderer.setStyle(this.elRef.nativeElement, 'color', 'black');
+  @HostListener('mouseleave') mouseleave(eventData: Event) {
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
+    // this.renderer.setStyle(this.elRef.nativeElement, 'color', 'black');
+
+
+    // using @HostBinding
+    this.backgroundColor = 'transparent';
+    this.color = 'black';
   }
 
 }
