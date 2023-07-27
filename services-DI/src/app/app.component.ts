21d347689aba9d9d81cf0d2e0169b4d6eb731247
kasunweerasinghe <kasunrachintha2018@gmail.com>
2023-07-27 11:28:03 +0530

    add account.service.ts

--- services-DI/src/app/app.component.ts
+++ services-DI/src/app/app.component.ts
@@ -1,31 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AccountsService} from "./accounts.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [AccountsService]
 })
-export class AppComponent {
-  accounts = [
-    {
-      name: 'Master Account',
-      status: 'active'
-    },
-    {
-      name: 'Test Account',
-      status: 'inactive'
-    },
-    {
-      name: 'Hidden Account',
-      status: 'unknown'
-    }
-  ];
+export class AppComponent implements OnInit {
+  accounts: { name: string, status: string }[] = [];
 
-  onAccountAdded(newAccount: {name: string, status: string}) {
-    this.accounts.push(newAccount);
+  // we use the constructor for use the service
+  constructor(private accountService: AccountsService) {
   }
 
-  onStatusChanged(updateInfo: {id: number, newStatus: string}) {
-    this.accounts[updateInfo.id].status = updateInfo.newStatus;
+  ngOnInit() {
+
+    // use service here
+    this.accounts = this.accountService.accounts;
+
   }
+
 }
--- services-DI/src/app/new-account/new-account.component.ts
+++ services-DI/src/app/new-account/new-account.component.ts
@@ -1,28 +1,28 @@
-import {Component, EventEmitter, Output} from '@angular/core';
+import {Component} from '@angular/core';
 import {LoggingService} from "../logging.service";
+import {AccountsService} from "../accounts.service";
 
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService] // in here we inform to angular how to create this service
+  providers: [LoggingService, AccountsService] // in here we inform to angular how to create this service
 })
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{ name: string, status: string }>();
+
 
   // Or We can use DI injection using inject() function:
   // we use the constructor for use the service
-  constructor(private loggingService: LoggingService) {}
+  constructor(private loggingService: LoggingService, private accountService: AccountsService) {
+  }
 
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
+    // console.log('A server status changed, new status: ' + accountStatus);
+
     // use service here
     this.loggingService.logStatusChange(accountStatus);
 
-    // console.log('A server status changed, new status: ' + accountStatus);
+    this.accountService.addAccount(accountName, accountStatus)
   }
 }
