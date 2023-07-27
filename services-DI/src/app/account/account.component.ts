c7dda942f4bb43d984b99206adfc0c139c698d21
kasunweerasinghe <kasunrachintha2018@gmail.com>
2023-07-27 10:24:10 +0530

    content update

--- services-DI/src/app/account/account.component.ts
+++ services-DI/src/app/account/account.component.ts
@@ -5,6 +5,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   templateUrl: './account.component.html',
   styleUrls: ['./account.component.css']
 })
+
 export class AccountComponent {
   @Input() account: {name: string, status: string};
   @Input() id: number;
