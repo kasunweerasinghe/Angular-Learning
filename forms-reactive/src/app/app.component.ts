f18aad26c02613431f5d1ca32ec94e99688f0cb2
kasunweerasinghe <kasunrachintha2018@gmail.com>
2023-07-28 10:29:00 +0530

    add validator for forms

--- forms-reactive/src/app/app.component.ts
+++ forms-reactive/src/app/app.component.ts
@@ -2,7 +2,7 @@ import {Component, OnInit} from '@angular/core';
 /*
 this is form package. this is a group of controls.
  */
-import {FormControl, FormGroup} from "@angular/forms";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -16,11 +16,17 @@ export class AppComponent implements OnInit {
   ngOnInit() {
     // in here we can initialize our form. we have to do that before rendering the form.
     this.signupForm = new FormGroup({
-      // need add controls. controls mean key and value pair that we pass to that form object group.
-      'username': new FormControl(null),
-      'email': new FormControl(null),
+      // need add controls('username','email'). controls mean key and value pair that we pass to that form object group.
+      'username': new FormControl(null, Validators.required),
+      //can add multiple Validators
+      'email': new FormControl(null, [Validators.required, Validators.email]),
       'gender': new FormControl('male'),
     });
+  }
+
+  // form submit on click
+  onSubmit() {
+    console.log(this.signupForm)
   }
 
 }
