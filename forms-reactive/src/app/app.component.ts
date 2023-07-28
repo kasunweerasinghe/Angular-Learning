810cca29e25500099aed40d9dc9c633126dc242a
kasunweerasinghe <kasunrachintha2018@gmail.com>
2023-07-28 10:39:17 +0530

    add nedted group form

--- forms-reactive/src/app/app.component.ts
+++ forms-reactive/src/app/app.component.ts
@@ -16,10 +16,13 @@ export class AppComponent implements OnInit {
   ngOnInit() {
     // in here we can initialize our form. we have to do that before rendering the form.
     this.signupForm = new FormGroup({
-      // need add controls('username','email'). controls mean key and value pair that we pass to that form object group.
-      'username': new FormControl(null, Validators.required),
-      //can add multiple Validators
-      'email': new FormControl(null, [Validators.required, Validators.email]),
+      // userData is nested form group
+      'userData': new FormGroup({
+        // need add controls('username','email'). controls mean key and value pair that we pass to that form object group.
+        'username': new FormControl(null, Validators.required),
+        //can add multiple Validators
+        'email': new FormControl(null, [Validators.required, Validators.email]),
+      }),
       'gender': new FormControl('male'),
     });
   }
