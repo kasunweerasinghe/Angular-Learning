9e7a3e41bdb4a95c0506dd0261f8b14fb17450c2
kasunweerasinghe <kasunrachintha2018@gmail.com>
2023-07-28 10:23:20 +0530

    content update

--- forms-reactive/src/app/app.component.ts
+++ forms-reactive/src/app/app.component.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
-// this is form package
-import {FormGroup} from "@angular/forms";
+import {Component, OnInit} from '@angular/core';
+/*
+this is form package. this is a group of controls.
+ */
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   genders = ['male', 'female'];
-  signFOrm: FormGroup;
+  signupForm: FormGroup;
+
+  ngOnInit() {
+    // in here we can initialize our form. we have to do that before rendering the form.
+    this.signupForm = new FormGroup({
+      // need add controls. controls mean key and value pair that we pass to that form object group.
+      'username': new FormControl(null),
+      'email': new FormControl(null),
+      'gender': new FormControl('male'),
+    });
+  }
 
 }
