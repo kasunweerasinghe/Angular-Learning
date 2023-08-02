222dcd45757d0ea8b40e5a60a4b25f8cba47f7f5
kasunweerasinghe <kasunrachintha2018@gmail.com>
2023-08-02 15:18:00 +0530

    load posts

--- http/src/app/app.component.ts
+++ http/src/app/app.component.ts
@@ -3,6 +3,7 @@ import {Component, OnInit} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 // the map operator allow us to get some data and return new data.
 import {map} from 'rxjs/operators'
+import {Post} from "./post.model";
 
 @Component({
   selector: 'app-root',
@@ -10,7 +11,7 @@ import {map} from 'rxjs/operators'
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  loadedPosts = [];
+  loadedPosts: Post[] = [];
 
   // after import HttpClientModule in module.ts HttpClient need to inject.
   constructor(private http: HttpClient) {
@@ -20,9 +21,9 @@ export class AppComponent implements OnInit {
     this.fetchPost();
   }
 
-  onCreatePost(postData: { title: string; content: string }) {
+  onCreatePost(postData: Post) {
     // Send Http request. posts.json is firebase option not angular
-    this.http.post('https://ng-complete-guide-f6ee1-default-rtdb.firebaseio.com/posts.json', postData).subscribe(responseData => {
+    this.http.post<{name: string}>('https://ng-complete-guide-f6ee1-default-rtdb.firebaseio.com/posts.json', postData).subscribe(responseData => {
       console.log(responseData)
     })
   }
@@ -37,19 +38,22 @@ export class AppComponent implements OnInit {
   }
 
   // fetch data from database.
+  // use observables operators to transform our data.
   private fetchPost() {
-    this.http.get('https://ng-complete-guide-f6ee1-default-rtdb.firebaseio.com/posts.json')
-      .pipe(map(responseData => {
-        const postsArray = [];
-        for (const key in responseData) {
-          if(responseData.hasOwnProperty(key)){
-            postsArray.push({...responseData[key], id: key})
+    this.http.get<{ [key: string]: Post }>('https://ng-complete-guide-f6ee1-default-rtdb.firebaseio.com/posts.json')
+      .pipe(
+        map(responseData => {
+          const postsArray: Post[] = [];
+          for (const key in responseData) {
+            if (responseData.hasOwnProperty(key)) {
+              postsArray.push({...responseData[key], id: key});
+            }
           }
-        }
-        return postsArray;
-      }))
+          return postsArray;
+        })
+      )
       .subscribe(posts => {
-        console.log(posts);
+        this.loadedPosts = posts;
       })
   }
 
